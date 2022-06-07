2132bd61950b8ee50fd56394b854e5f9625f3b5e
blade_ly <blade_ly@aliyun.com>
2022-06-07 17:30:00 +0800

    create account

--- tools/account/account.go
+++ tools/account/account.go
@@ -37,13 +37,15 @@ func main() {
 			Usage:    "create a new account",
 			Category: "account",
 			Action: func(c *cli.Context) error {
-				fmt.Printf("\n %c[1;40;31m%s%c[0m\n\n", 0x1B, "testPrintColor", 0x1B)
 				fmt.Println("please input your password")
 				fmt.Scanln(&passwordfirst)
 				fmt.Println("please input your password again")
 				fmt.Scanln(&passwordagian)
 				if passwordagian == passwordfirst {
-					cpchain.NewKeystore(password)
+					err := cpchain.CreateKeystore(password)
+					if err != nil {
+						fmt.Printf("%c[0;40;31m%s%c[0m", 0x1b, "ERROR: create new keystore failed", 0x1b)
+					}
 				} else {
 					fmt.Printf("%c[0;40;31m%s%c[0m", 0x1b, "ERROR: the password did not match the re-typed password", 0x1b)
 				}
