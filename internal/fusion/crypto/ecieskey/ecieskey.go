4ca57da4d44b5bb39de5abf6cc6536797e84f3f1
blade_ly <blade_ly@aliyun.com>
2022-06-09 10:58:59 +0800

    ecieskey

--- internal/fusion/crypto/ecieskey/ecieskey.go
+++ internal/fusion/crypto/ecieskey/ecieskey.go
@@ -1,29 +1,30 @@
 package ecieskey
 
-// import (
-// 	"crypto/ecdsa"
-// 	"crypto/rand"
-
+import (
+	"crypto/ecdsa"
+	"github.com/CPChain/cpchain-golang-sdk/internal/fusion/crypto/ecies"
+	"github.com/CPChain/cpchain-golang-sdk/internal/fusion/crypto"
+)
 // 	"github.com/ethereum/go-ethereum/common/hexutil"
 // 	"github.com/ethereum/go-ethereum/crypto"
-// 	"github.com/ethereum/go-ethereum/crypto/ecies"
+// 	"github.com//go-ethereum/crypto/ecies"
 // )
 
 // func Encrypt(publicKey *ecies.PublicKey, data []byte) (m []byte, err error) {
 // 	return ecies.Encrypt(rand.Reader, publicKey, data, nil, nil)
 // }
 
-// func Decrypt(privateKey *ecies.PrivateKey, encryptedData []byte) (m []byte, err error) {
-// 	return privateKey.Decrypt(encryptedData, nil, nil)
-// }
+func Decrypt(privateKey *ecies.PrivateKey, encryptedData []byte) (m []byte, err error) {
+	return privateKey.Decrypt(encryptedData, nil, nil)
+}
 
 // func DecodeEcdsaPubKeyFrom(ecdsaPublicKeyBytes []byte) (*ecdsa.PublicKey, error) {
 // 	return crypto.UnmarshalPubkey(ecdsaPublicKeyBytes)
 // }
 
-// func EncodeEcdsaPubKey(pub *ecdsa.PublicKey) []byte {
-// 	return crypto.FromECDSAPub(pub)
-// }
+func EncodeEcdsaPubKey(pub *ecdsa.PublicKey) []byte {
+	return crypto.FromECDSAPub(pub)
+}
 
 // func EncodeEcdsaPrivateKey(prv *ecdsa.PrivateKey) []byte {
 // 	return crypto.FromECDSA(prv)
