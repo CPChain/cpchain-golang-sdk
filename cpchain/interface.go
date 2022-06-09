03884551f782004f96c3a54d0ab814da0204f5c5
blade_ly <blade_ly@aliyun.com>
2022-06-09 18:09:23 +0800

    update

--- cpchain/interface.go
+++ cpchain/interface.go
@@ -2,9 +2,15 @@ package cpchain
 
 import (
 	"math/big"
+	"strings"
+	"crypto/ecdsa"
 
+	"github.com/pborman/uuid"
 	"github.com/CPChain/cpchain-golang-sdk/internal/fusion"
+	"github.com/CPChain/cpchain-golang-sdk/internal/fusion/common"
 	"github.com/CPChain/cpchain-golang-sdk/internal/fusion/contract"
+	"github.com/CPChain/cpchain-golang-sdk/internal/fusion/types"
+	"github.com/CPChain/cpchain-golang-sdk/internal/fusion/crypto/ecies"
 )
 
 type Event = contract.Event
@@ -49,3 +55,51 @@ type CPChain interface {
 }
 
 // TODO simulate chain
+
+type Account struct {
+	Address common.Address `json:"address"` // cpchain account address derived from the key
+	URL     URL            `json:"url"`     // Optional resource locator within a backend
+}
+
+type URL struct {
+	Scheme string // Protocol scheme to identify a capable account backend
+	Path   string // Path for the backend to identify a unique entity
+}
+
+func (u URL) Cmp(url URL) int {
+	if u.Scheme == url.Scheme {
+		return strings.Compare(u.Path, url.Path)
+	}
+	return strings.Compare(u.Scheme, url.Scheme)
+}
+
+type Wallet interface{
+	URL() URL
+
+	Account() []Account
+
+	SignTxWithPassphrase(account Account, passphrase string, tx *types.Transaction, chainID *big.Int) (*types.Transaction, error)
+}
+
+
+type KeyStore struct {
+	storage  keyStore                     // Storage backend, might be cleartext or encrypted
+	// cache    *accountCache                // In-memory account cache over the filesystem storage
+}
+
+type keyStore interface {
+	GetKey(addr common.Address, filename string, auth string) (*Key, error)
+}
+
+type Key struct {
+	Id uuid.UUID // Version 4 "random" for unique id not derived from key data
+	// to simplify lookups we also store the address
+	Address common.Address
+	// we only store privkey as pubkey/address can be derived from it
+	// privkey in this struct is always in plaintext
+	PrivateKey *ecdsa.PrivateKey
+
+	// we cache ecies.PrivateKey used to decrypt data that encrypted by ecies.PublicKey from remote node
+	EciesPrivateKey *ecies.PrivateKey
+}
+
--- cpchain/keystore.go
+++ cpchain/keystore.go
@@ -1,18 +1,82 @@
 package cpchain
 
 import (
-	"fmt"
-	"log"
-
-	"github.com/ethereum/go-ethereum/accounts/keystore" //TOOD 是否要换成fusion
+	"crypto/ecdsa"
+	"os"
+	"path/filepath"
+	"errors"
+	"github.com/CPChain/cpchain-golang-sdk/internal/fusion/crypto"
+	"github.com/CPChain/cpchain-golang-sdk/internal/fusion/common"
+	"github.com/CPChain/cpchain-golang-sdk/internal/accounts"
+	"github.com/CPChain/cpchain-golang-sdk/internal/keystore"
 )
 
-func NewKeystore(password string) {
-	ks := keystore.NewKeyStore("./keystore", keystore.StandardScryptN, keystore.StandardScryptP)
-	account, err := ks.NewAccount(password)
+
+// func Connect(password string) (*cpclient.Client, error, *ecdsa.PrivateKey, *ecdsa.PublicKey, common.Address, *keystore.KeyStore, accounts.Account, *big.Int) {
+// 	ep, err := configs.ResolveUrl(endPoint)
+// 	if err != nil {
+// 		// log.Fatal("unknown endpoint", "endpoint", endPoint, "err", err)
+// 	}
+// 	// Create client.
+// 	client, err := cpclient.Dial(ep)
+// 	if err != nil {
+// 		// log.Fatal(err.Error())
+// 	}
+
+// 	chainConfig, err := client.ChainConfig()
+// 	if err != nil {
+// 		// log.Fatal(err.Error())
+// 	}
+// 	chainId, runMode := chainConfig.ChainID.Uint64(), configs.Mainnet
+// 	switch chainId {
+// 	case configs.DevChainId:
+// 		runMode = configs.Dev
+// 	case configs.MainnetChainId:
+// 		runMode = configs.Mainnet
+// 	case configs.TestMainnetChainId:
+// 		runMode = configs.TestMainnet
+// 	case configs.TestnetChainId:
+// 		runMode = configs.Testnet
+// 	default:
+// 		log.Fatal("unknown chain id")
+// 	}
+// 	configs.SetRunMode(runMode)
+
+// 	privateKey, publicKeyECDSA, fromAddress, kst, account, err := DecryptKeystore(password)
+// 	if err != nil {
+// 		log.Fatal(err.Error())
+// 	}
+// 	return client, err, privateKey, publicKeyECDSA, fromAddress, kst, account, big.NewInt(0).SetUint64(chainId)
+// }
+
+
+func DecryptKeystore(keyStoreFilePath string, password string) (*ecdsa.PrivateKey, *ecdsa.PublicKey, common.Address, *keystore.KeyStore, accounts.Account, error) {
+	// Open keystore file.
+	file, err := os.Open(keyStoreFilePath)
 	if err != nil {
-		log.Fatal(err)
+		return nil, nil, [20]byte{}, nil, accounts.Account{}, err
+	}
+	keyPath, err := filepath.Abs(filepath.Dir(file.Name()))
+	if err != nil {
+		return nil, nil, [20]byte{}, nil, accounts.Account{}, err
+	}
+	// Create keystore and get account.
+	kst := keystore.NewKeyStore(keyPath, 2, 1)
+	account := kst.Accounts()[0]
+	account, key, err := kst.GetDecryptedKey(account, password)
+	if err != nil {
+		return nil, nil, [20]byte{}, nil, accounts.Account{}, err
+	}
+	// Get private and public keys.
+	privateKey := key.PrivateKey
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		return nil, nil, [20]byte{}, nil, accounts.Account{}, errors.New("error casting public key to ECDSA")
 	}
 
-	fmt.Println(account.Address.Hex())
+	// Get contractAddress.
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	return privateKey, publicKeyECDSA, fromAddress, kst, account, nil
+
 }
